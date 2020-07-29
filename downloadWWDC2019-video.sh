d47df615998c7aa145b443ca92290046d09f0388
Eric Golovin <ericgolovin@Erics-MacBook-Pro.local>
2020-07-29 22:01:42 +0300

    Add fix

--- downloadWWDC2019-video.sh
+++ downloadWWDC2019-video.sh
@@ -1,7 +1,7 @@
 ARG=$1
 NAME=$2
-ffmpeg -i "$ARG/2161c/prog_index.m3u8" -c copy "temp.mp4"
-ffmpeg -i "$ARG/audioc/prog_index.m3u8" -c copy "temp.aac"
+ffmpeg -i "$ARG/2161c/prog_index.m3u8" -c copy "temp$NAME.mp4"
+ffmpeg -i "$ARG/audioc/prog_index.m3u8" -c copy "temp$NAME.aac"
 ffmpeg -i "temp.mp4" -i "temp.aac" -c copy "$NAME.mp4"
-rm "temp.mp4"
-rm "temp.aac"
+rm "temp$NAME.mp4"
+rm "temp$NAME.aac"
