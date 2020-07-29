8929268d77f83a578af5a1ac17d1fad2b4aa26cb
Eric Golovin <ericgolovin@outlook.com>
2020-07-29 22:50:18 +0300

    fix

--- downloadWWDC2019-video.sh
+++ downloadWWDC2019-video.sh
@@ -2,6 +2,6 @@ ARG=$1
 NAME=$2
 ffmpeg -i "$ARG/2161c/prog_index.m3u8" -c copy "temp$NAME.mp4"
 ffmpeg -i "$ARG/audioc/prog_index.m3u8" -c copy "temp$NAME.aac"
-ffmpeg -i "temp.mp4" -i "temp.aac" -c copy "$NAME.mp4"
+ffmpeg -i "temp$NAME.mp4" -i "temp$NAME.aac" -c copy "$NAME.mp4"
 rm "temp$NAME.mp4"
 rm "temp$NAME.aac"
